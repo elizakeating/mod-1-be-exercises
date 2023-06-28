ce4248a0e17969d7e7918e5cee7d52ab136a357a
Eliza Keating <ek.elizakeating@gmail.com>
2023-06-28 12:35:19 -0600

    Add pirate class to lib and finish pirate tests

---
+++ ruby_exercises/mythical-creatures/lib/pirate.rb
@@ -0,0 +1,28 @@
+class Pirate
+
+    attr_reader :name, :job, :heinous_act, :booty
+
+    def initialize(name, job = "Scallywag")
+        @name = name
+        @job = job
+        @heinous_act = 0
+        @booty = 0
+    end
+
+    def commit_heinous_act
+        @heinous_act += 1
+    end
+
+    def cursed?
+        if @heinous_act >= 3
+            true
+        else
+            false
+        end
+    end
+
+    def rob_ship 
+        @booty += 100
+    end
+
+end
--- ruby_exercises/mythical-creatures/spec/pirate_spec.rb
+++ ruby_exercises/mythical-creatures/spec/pirate_spec.rb
@@ -39,13 +39,23 @@ RSpec.describe Pirate do
 
   it 'has a booty' do
     # create a pirate
+    pirate = Pirate.new("Jack")
     # check that the pirate starts with 0 booty
+    expect(pirate.booty).to eq(0)
   end
 
   it 'gets 100 booty for robbing a ship' do
     # create a pirate
+    pirate = Pirate.new("Jack")
     # rob some ships
     # check that the pirate got 100 booty for each ship it robbed
+    pirate.rob_ship
+    expect(pirate.booty).to eq(100)
+    pirate.rob_ship
+    expect(pirate.booty).to eq(200)
+    pirate.rob_ship
+    pirate.rob_ship
+    expect(pirate.booty).to eq(400)
   end
 
 end
