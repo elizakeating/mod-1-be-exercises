d8bd2be5e583d73ec715ba0e9db6f7e120e73a9b
Eliza Keating <ek.elizakeating@gmail.com>
2023-07-06 10:32:01 -0600

    Pass student tests based on interaction pattern provided

--- lessons/intro_to_testing/spec/student_spec.rb
+++ lessons/intro_to_testing/spec/student_spec.rb
@@ -1,13 +1,32 @@
-# Add the 2 requires you will need here
+#only need to require rspec for before ruby 3
+require "rspec"
+require "./lib/student"
 
 
 describe Student do
   describe '#initialize' do
     it 'is an instance of student' do
-      # write the code to initialize a new student object
+      student = Student.new("Penelope")
+
+      expect(student).to be_an_instance_of(Student)
     end
-    # test it has a name
-    # test it has cookies
-    # test it can add cookies
+    it 'has readable attributes' do
+      # test it has a name
+      # test it has cookies
+      student = Student.new("Penelope")
+
+      expect(student.name).to eq("Penelope")
+      expect(student.cookies).to eq([])
+    end
+  end
+  describe '#add_cookie' do
+    it 'can add cookies' do
+      # test it can add cookies
+      student = Student.new("Penelope")
+      student.add_cookie("Chocolate Chunk")
+      student.add_cookie("Snickerdoodle")
+
+      expect(student.cookies).to eq(["Chocolate Chunk", "Snickerdoodle"])
+    end
   end
 end
