8fea1d9166b0d0ff8f763cc5535ed636819e894c
Eliza Keating <ek.elizakeating@gmail.com>
2023-07-12 20:51:02 -0600

    Complete venue ic problems

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,2 +1,33 @@
 class Venue
+  attr_reader :name, :capacity, :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(name)
+    @patrons << name
+  end
+
+  def yell_at_patrons
+    yelling = @patrons.map do |patron|
+      patron.upcase
+    end
+  end
+
+  def over_capacity?
+    if @patrons.size > capacity
+      true
+    else
+      false
+    end
+  end
+
+  def kick_out
+    while @patrons.size > capacity
+      @patrons.pop
+    end
+  end
 end
