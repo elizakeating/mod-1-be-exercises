40cbaa1595912a1e4a3a928bf246cdab6f87e708
Eliza Keating <ek.elizakeating@gmail.com>
2023-07-24 11:12:37 -0600

    nested iteration warm up

--- lessons/nested_iteration/lesson/exercise/cities_lived_in.rb
+++ lessons/nested_iteration/lesson/exercise/cities_lived_in.rb
@@ -10,22 +10,44 @@ cities_lived_in = {
 # Get a unique list of all of the cities that these humans have lived in  
 # ["Philadelphia", "Fort Collins", "Seattle", "Denver", "Santa Fe", "Portland", "Lansing", "Columbus", "Austin"]
 
+# p cities_lived_in.values.flatten.uniq
 
+all_cities = []
 
+cities_lived_in.each do |person, cities|
+    cities.each do |city|
+        unless all_cities.include?(city)
+            all_cities.push(city)
+        end
+    end
+end
+
+p all_cities
 
 # Problem #2: 
 # Write code that iterates through the `cities_lived_in` hash, and returns a list of  
 # names of the humans who've lived in Philadelphia.
 
-# [:michaela, :mike, :salvador]
+# hashes two block variables
+
+philadelphia_people = []
+
+cities_lived_in.each do |people, cities|
+    philadelphia_people << people if cities.include?("Philadelphia")
+end
+
+p philadelphia_people
+
+# p cities_lived_in.map do { |people, cities| cities.include?("Philadelphia") }
+# [:michaela, :mike, :alex]
 # or
-# ["Michaela", "Mike", "Salvador"]
+# ["Michaela", "Mike", "Alex"]
 
 
 
 
 # Problem #3: 
-# Create a hash that has the city as a key, and the number of people that live in it as it's value: 
+# Create a hash that has the city as a key,r and the number of people that live in it as it's value: 
 
 # {
 #     "Philadelphia" => 3,
@@ -38,3 +60,33 @@ cities_lived_in = {
 #     "Columbus => 1,
 #     "Austin" => 1
 # }
+
+# cities_population = {}
+
+# cities_lived_in.each do |person|
+#     person[1].each do |city|
+#         if !cities_population.keys.include?(city)
+#             cities_population[city] = 1
+#         else
+#             cities_population[city] += 1
+#         end
+#     end
+# end
+
+# p cities_population
+
+cities_population = {}
+
+cities_lived_in.each do |person, cities|
+    cities.each do |city|
+        if !cities_population.keys.include?(city)
+            cities_population[city] = 1
+        else
+            cities_population[city] += 1
+        end
+    end
+end
+
+p cities_population
+
+p cities_lived_in.values.flatten.tally
--- lessons/nested_iteration/lesson/warmup/warmup.rb
+++ lessons/nested_iteration/lesson/warmup/warmup.rb
@@ -1,13 +1,101 @@
+# Part One
+# Given the follow array:
+animals = [:dog, :cat, :zebra, :quokka, :unicorn, :bear]
+
+# Use an enumerable to
+# 1. Return an array of animals as strings.
+
+# animal_strings = animals.map do |animal|
+#   animal.to_s
+# end
+
+# p animal_strings
+
+# p animals.map { |animal| animal.to_s }
+
+# 2. Return an array of animals with a length >= 4.
+
+# large_animals = animals.find_all do |animal|
+#   animal.length >= 4
+# end
+
+# p large_animals
+
+# p animals.find_all { |animal| animal.length >=4 }
+
+# Part Two
 # Given the following array:
 nested_animals = [[:dog, :cat, :zebra], [:quokka, :unicorn, :bear]]
 
 # Use an enumerable to:
 # 1. Return an unnested (single layer) array of animals as strings
 
+# nested_animals.map do |nested|
+#   p nested.map do |animal|
+#     animal_string << animal.to_s
+#   end
+# end
 
+# p animal_string
+
+# animal_string = nested_animals.map do |animals|
+#   animals.map do |animal|
+#     animal.to_s
+#   end
+# end
+
+# p animal_string.flatten
+
+# animal_string = []
+
+# nested_animals.each do |animals|
+#   animals.each do |animal|
+#     animal_string << animal.to_s
+#   end
+# end
+
+# p animal_string
 
 # 2. Return an unnested array of animals with length >= 4
 
+# animal_four = nested_animals.map do |animals|
+#   animals.find_all do |animal|
+#     animal.length >= 4
+#   end
+# end
 
+# p animal_four.flatten
+
+# p nested_animals.flatten.find_all { |animal| animal.length >= 4 }
+
+# animal_string = []
+
+# nested_animals.each do |animals|
+#   animals.each do |animal|
+#     animal_string << animal if animal.length >= 4
+#   end
+# end
+
+# p animal_string
 
 # 3. Return a hash where the keys are the animal, and the values are the length. ex: {dog: 3, cat: 3, zebra: 5 ... }
+
+animal_hash = {}
+animal_hash = nested_animals.flatten.map { |animal| animal_hash[animal] = animal.length }
+p animal_hash
+
+# animal_hash = {}
+
+# nested_animals.each do |group|
+#   group.each do |animal|
+#     animal_hash[animal]= animal.length
+#   end
+# end
+
+# p animal_hash
+
+# objects = nested_animals.flatten.each_with_object({}) do |animal, hash|
+#   hash[animal] = animal.length
+# end
+
+# p objects
