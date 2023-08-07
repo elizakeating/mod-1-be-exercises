d23be93201301fb39c5484073eba451b366cc07a
Eliza Keating <ek.elizakeating@gmail.com>
2023-08-07 16:36:19 -0600

    test: fix before each loop on artist tests

--- practice_independent_challenges/week5/museo/spec/artist_spec.rb
+++ practice_independent_challenges/week5/museo/spec/artist_spec.rb
@@ -2,7 +2,7 @@ require "./lib/artist"
 
 RSpec.describe Artist do
   before(:each) do
-    attributes = {
+    @attributes = {
       id: "2",
       name: "Ansel Adams",
       born: "1902",
@@ -10,47 +10,48 @@ RSpec.describe Artist do
       country: "United States"
     }
 
-    artist = Artist.new(attributes)
+    @artist = Artist.new(@attributes)
   end
 
   describe "#initialize" do
     it "exists" do
-      expect(artist).to be_a(Artist)
+      expect(@artist).to be_a(Artist)
     end
   end
 
   describe "#id" do
     it "returns id of artist" do
-      expect(artist.id).to eq("2")
+      expect(@artist.id).to eq("2")
     end
   end
 
   describe "#name" do
     it "returns name of artist" do
-      expect(artist.name).to eq("Ansel Adams")
+      expect(@artist.name).to eq("Ansel Adams")
     end
   end
 
   describe "#born" do
     it "returns year the artist was born" do
-      expect(artist.born).to eq("1902")
+      expect(@artist.born).to eq("1902")
     end
   end
 
   describe "#died" do
     it "returns year artist died" do
-       expect(artist.died).to eq("1984")
+       expect(@artist.died).to eq("1984")
+    end
   end
 
   describe "#country" do
     it "returns country of artist" do
-      expect(artist.country).to eq("United States")
+      expect(@artist.country).to eq("United States")
     end
   end
 
   describe "#age_at_death" do
     it "returns age at which the artist died" do
-      expect(artist.age_at_death).to eq(82)
+      expect(@artist.age_at_death).to eq(82)
     end
   end
 end
