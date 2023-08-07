1b47d2500a3c7fac63e11325ff415702a2e8133b
Eliza Keating <ek.elizakeating@gmail.com>
2023-08-07 16:33:26 -0600

    test: fix before each loop

--- practice_independent_challenges/week5/museo/spec/photograph_spec.rb
+++ practice_independent_challenges/week5/museo/spec/photograph_spec.rb
@@ -3,43 +3,43 @@ require "./lib/photograph"
 RSpec.describe Photograph do
 
   before(:each) do
-    attributes = {
+    @attributes = {
       id: "1",
       name: "Rue Mouffetard, Paris (Boy with Bottles)",
       artist_id: "4",
       year: "1954"
     }
 
-    photograph = Photograph.new(attributes)
+    @photograph = Photograph.new(@attributes)
   end
 
   describe "#initialize" do
     it "exists" do
-      expect(photograph).to be_a(Photograph)
+      expect(@photograph).to be_a(Photograph)
     end
   end
 
   describe "#id" do
     it "returns id of the photograph" do
-      expect(photograph.id).to eq("1")
+      expect(@photograph.id).to eq("1")
     end
   end
 
   describe "#name" do
     it "returns name of the photograph" do
-      expect(photograph.name).to eq("Rue Mouffetard, Paris (Boy with Bottles)")
+      expect(@photograph.name).to eq("Rue Mouffetard, Paris (Boy with Bottles)")
     end
   end
 
   describe "#artist_id" do
     it "returns artist id of the photograph" do
-      expect(photograph.artist_id).to eq("4")
+      expect(@photograph.artist_id).to eq("4")
     end
   end
 
   describe "#year" do
     it "returns year of photograph" do
-      expect(photograph.year).to eq("1954")
+      expect(@photograph.year).to eq("1954")
     end
   end
 end
