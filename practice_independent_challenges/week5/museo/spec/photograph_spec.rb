21280cf7614f182c7db52017756348c9fb824e7c
Eliza Keating <ek.elizakeating@gmail.com>
2023-08-07 16:23:34 -0600

    test: add tests for photograph class for iteration 1

---
+++ practice_independent_challenges/week5/museo/spec/photograph_spec.rb
@@ -0,0 +1,45 @@
+require "./lib/photograph"
+
+RSpec.describe Photograph do
+
+  before(:each) do
+    attributes = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "4",
+      year: "1954"
+    }
+
+    photograph = Photograph.new(attributes)
+  end
+
+  describe "#initialize" do
+    it "exists" do
+      expect(photograph).to be_a(Photograph)
+    end
+  end
+
+  describe "#id" do
+    it "returns id of the photograph" do
+      expect(photograph.id).to eq("1")
+    end
+  end
+
+  describe "#name" do
+    it "returns name of the photograph" do
+      expect(photograph.name).to eq("Rue Mouffetard, Paris (Boy with Bottles)")
+    end
+  end
+
+  describe "#artist_id" do
+    it "returns artist id of the photograph" do
+      expect(photograph.artist_id).to eq("4")
+    end
+  end
+
+  describe "#year" do
+    it "returns year of photograph" do
+      expect(photograph.year).to eq("1954")
+    end
+  end
+end
