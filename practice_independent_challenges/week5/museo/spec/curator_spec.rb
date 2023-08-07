a847eec386bcc74a2d77a5d7868d74225b7c7ed6
Eliza Keating <ek.elizakeating@gmail.com>
2023-08-07 16:50:23 -0600

    test: fix before each loop for curator class

--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -4,23 +4,23 @@ require "./lib/curator"
 
 RSpec.describe Curator do
   before(:each) do
-    curator = Curator.new
+    @curator = Curator.new
 
-    photo_1 = Photograph.new({
+    @photo_1 = Photograph.new({
       id: "1",      
       name: "Rue Mouffetard, Paris (Boy with Bottles)",      
       artist_id: "1",      
       year: "1954"      
     })
 
-    photo_2 = Photograph.new({
+    @photo_2 = Photograph.new({
       id: "2",      
       name: "Moonrise, Hernandez",      
       artist_id: "2",      
       year: "1941"      
     })
 
-    artist_1 = Artist.new({
+    @artist_1 = Artist.new({
       id: "1",      
       name: "Henri Cartier-Bresson",      
       born: "1908",      
@@ -28,7 +28,7 @@ RSpec.describe Curator do
       country: "France"      
     })
 
-    artist_2 = Artist.new({
+    @artist_2 = Artist.new({
       id: "2",      
       name: "Ansel Adams",      
       born: "1902",      
@@ -39,46 +39,46 @@ RSpec.describe Curator do
 
   describe "#initialize" do
     it "exists" do
-      expect(curator).to be_a(Curator)
+      expect(@curator).to be_a(Curator)
     end
   end
 
   describe "#photographs" do
     it "returns the photographs of the curator" do
-      expect(curator.photographs).to eq([])
+      expect(@curator.photographs).to eq([])
     end
   end
 
   describe "#artists" do
     it "returns the artists of the curator" do
-      expect(curator.artists).to eq([])
+      expect(@curator.artists).to eq([])
     end
   end
 
   describe "#add_photograph" do
     it "adds a photograph to the curator" do
-      curator.add_photograph(photo_1)
-      curator.add_photograph(photo_2)
+      curator.add_photograph(@photo_1)
+      curator.add_photograph(@photo_2)
 
-      expect(curator.photographs).to eq([photo_1, photo_2])
+      expect(@curator.photographs).to eq([@photo_1, @photo_2])
     end
   end
 
   describe "#add_artist" do
     it "adds an artist to the curator" do
-      curator.add_photograph(artist_1)
-      curator.add_photograph(artist_2)
+      curator.add_photograph(@artist_1)
+      curator.add_photograph(@artist_2)
 
-      expect(curator.artists).to eq([artist_1, artist_2])
+      expect(@curator.artists).to eq([@artist_1, @artist_2])
     end
   end
 
   describe "#find_artist_by_id" do
     it "returns an artist based off of artist id" do
-      curator.add_photograph(artist_1)
-      curator.add_photograph(artist_2)
+      curator.add_photograph(@artist_1)
+      curator.add_photograph(@artist_2)
 
-      expect(curator.find_artist_by_id("1")).to eq(artist_1)
+      expect(@curator.find_artist_by_id("1")).to eq(@artist_1)
     end
   end
 end
