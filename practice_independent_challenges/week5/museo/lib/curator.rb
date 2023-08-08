b31f4d82532f5ee47d0e0dfeb8b382737e2f253e
Eliza Keating <ek.elizakeating@gmail.com>
2023-08-07 20:54:54 -0600

    feat: add artist_photograph_list, artists_more_than_one_photo, country_photographs methods to curator class

--- practice_independent_challenges/week5/museo/lib/curator.rb
+++ practice_independent_challenges/week5/museo/lib/curator.rb
@@ -21,24 +21,59 @@ class Curator
     end
   end
 
-  # def artist_photograph_list
-  #   list = Hash.new([])
-  #   @artists.find_all do |artist|
-  #     @photographs.find_all do |photo|
-  #       if photo.artist_id == artist.id
-  #         list[artist] => list[artist] << photo
-  #         require 'pry';binding.pry
-  #       end
-  #     end
-  #   end
-  # end
+  def artist_photograph_list
+    list = Hash.new([])
+    
+    @artists.each do |artist|
+      artist_photos = []
+      @photographs.each do |photo|
+        if photo.artist_id == artist.id
+          artist_photos << photo
+        end
+      end
+
+      list[artist] = artist_photos
+    end
+
+    list
+  end
 
   def artists_more_than_one_photo
-    artist_list = []
     photograph_count = Hash.new(0)
+    artist_list = []
 
-    @artists.map do |artist|
-      
+    @artists.each do |artist|
+      photographs.each do |photo|
+        if photo.artist_id == artist.id
+          photograph_count[artist.name] += 1
+        end
+      end
     end
+    
+    photograph_count.each do |artist, photo_count|
+      if photo_count > 1
+        artist_list << artist
+      end
+    end
+
+    artist_list
+  end
+
+  def country_photographs(country)
+    photos_from_country = []
+
+    artist_from_country = @artists.find_all do |artist|
+      artist.country == country
+    end
+
+    artist_from_country.each do |artist|
+      @photographs.each do |photo|
+        if photo.artist_id == artist.id
+          photos_from_country << photo
+        end
+      end
+    end
+
+    photos_from_country
   end
 end
